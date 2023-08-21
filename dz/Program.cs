d3144dc1eb2ccefb36c5f889baba2718d282e6cc
user <user@WIN-OPHCB63MAB6>
2023-08-21 21:30:23 +0400

    Add project files.

---
+++ dz/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task 1
+// Console.WriteLine("It's easy to win forgiveness for being wrong;\n\t being right is what gets you into real trouble.\n\t\t\t\t\t Bjarne Stroustrup");
+
+// Task 2
+//int firstDigit = 90;
+//int secondDigit = 10;
+//Console.WriteLine(secondDigit * firstDigit / 100);
+
+// Task 3
+//int fiDigit = 1;
+//int seDigit = 5;
+//int thDigit = 7;
+//int foDigit = 8;
+
+//Console.WriteLine(fiDigit * 1000 + seDigit * 100 + thDigit * 10 + foDigit * 1);
